9dcafc60c99aaf5cb1a2d44a6ca7ad2dc6b6b073
Joao Thompson <joaopthompson@gmail.com>
2019-08-19 14:13:30 -0300

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'questions_controller/ask'
+  get 'questions_controller/answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
